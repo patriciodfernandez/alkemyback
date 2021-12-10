8f85fa1ad72880e143fc3952538df00dc2654fd4
patricio.dfernandez <patricio.dfernandez@gmail.com>
2021-12-10 18:26:42 -0300

    as

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 //ajskdbnaskjndszkjdnakjsdnkjasnd
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://uddsfxkjteikhq:67232b55c5bf8b1085d8e9fe6497afe138fadc95e4acbed0c72343a618d8db48@ec2-18-210-159-154.compute-1.amazonaws.com:5432/d4ej20rk0ik1t3", {
+const db = new Sequelize("postgres://yqfpkwfzeegyvo:46b05c5e2cc7758b8272526c6bc86961f269e80c0e039ad915da90a76703470e@ec2-54-198-213-75.compute-1.amazonaws.com:5432/de8vhmcbsui7j1", {
   logging: true, // set to console.log to see the raw SQL queries
 
 });
+
 
 
 module.exports = db;
